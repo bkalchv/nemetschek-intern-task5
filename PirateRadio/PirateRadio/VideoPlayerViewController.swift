c110661c8eae51fca47473d88a6db96e93ef76be
bkalchv <bkaltchev98@gmail.com>
2022-05-19 12:39:48 +0300

    adds a webView and downloads a video, when API-button clicked

--- PirateRadio/PirateRadio/VideoPlayerViewController.swift
+++ PirateRadio/PirateRadio/VideoPlayerViewController.swift
@@ -5,20 +5,93 @@
 //  Created by Bogdan Kalchev on 11.04.22.
 //
 
+import WebKit
 import UIKit
 import YouTubeiOSPlayerHelper
 
-class VideoPlayerViewController: UIViewController {
+class VideoPlayerViewController: UIViewController, WKUIDelegate, WKDownloadDelegate, WKNavigationDelegate {
     
+    @IBOutlet weak var downloadButtonsWebView: WKWebView!
     @IBOutlet weak var downloadMP3Button: UIButton!
     @IBOutlet weak var ytPlayerView: YTPlayerView!
     var videoId: String = ""
     var downloadDataTask: URLSessionDataTask? = nil
     
+    private var filePathLocalDestination: URL?
+    //weak var downloadDelegate: WebDownloadable?
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         ytPlayerView.load(withVideoId: videoId)
+        
+        let myURL = URL(string: "https://api.vevioz.com/api/button/videos/\(videoId)") // api.vevioz buttons (-) have lots of ads
+    
+        let myRequest = URLRequest(url: myURL!)
+        downloadButtonsWebView.load(myRequest)
+        downloadButtonsWebView.navigationDelegate = self
+        
+//        if let youtubeWatchURL = configureYoutubeWatchURL(videoId: videoId) {
+//            let myURL = URL(string: "https://yt-download.org/api/widget?url=\(youtubeWatchURL)") // yt-download.org widget
+//            print(myURL!.absoluteString)
+//            let myRequest = URLRequest(url: myURL!)
+//            downloadButtonsWebView.load(myRequest)
+//            downloadButtonsWebView.navigationDelegate = self
+//        }
+    }
+    
+    // MARK: WKNavigationDelegate's methods
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, preferences: WKWebpagePreferences, decisionHandler: @escaping (WKNavigationActionPolicy, WKWebpagePreferences) -> Void) {
+        if navigationAction.shouldPerformDownload {
+            decisionHandler(.download, preferences)
+        } else {
+            decisionHandler(.allow, preferences)
+        }
+    }
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse, decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
+        
+        let responseUrl = navigationResponse.response.url
+        print(responseUrl!.absoluteString)
+        
+        if responseUrl!.absoluteString.contains("\(videoId)") && responseUrl!.absoluteString != "https://api.vevioz.com/api/button/videos/\(videoId)" {
+            decisionHandler(.download)
+            return
+        }
+        
+        if navigationResponse.canShowMIMEType {
+            //print(navigationResponse)
+            decisionHandler(.allow)
+        } else {
+            decisionHandler(.download)
+        }
+    }
+    
+    // MARK: WKDonwloadDelegate's methods
+    
+    func webView(_ webView: WKWebView, navigationAction: WKNavigationAction, didBecome download: WKDownload) {
+        download.delegate = self
+    }
+    
+    func webView(_ webView: WKWebView, navigationResponse: WKNavigationResponse, didBecome download: WKDownload) {
+        download.delegate = self
+    }
+    
+    func download(_ download: WKDownload, decideDestinationUsing response: URLResponse, suggestedFilename: String, completionHandler: @escaping (URL?) -> Void) { // have it
+        
+        let downloadsDirectory = Constants.YOUTUBE_TO_MP3_DOWNLOADS_DIRECTORY_URL
+        
+        filePathLocalDestination = downloadsDirectory.appendingPathComponent(suggestedFilename)
+        
+        print(filePathLocalDestination!.absoluteString)
+        
+        completionHandler(filePathLocalDestination!)
+    }
+    
+    func downloadDidFinish(_ download: WKDownload) {
+        print("Download finished")
     }
     
     func configureYoutubeWatchURL(videoId: String) -> URL? {
@@ -58,7 +131,7 @@ class VideoPlayerViewController: UIViewController {
         }
     }
     
-    func downloadFile(url: URL, filename: String) {
+    func downloadMP3(url: URL, filename: String) {
         self.createYoutubeToMp3DownloadsDirectoryInCacheIfNonExistent()
         
         let fileLocalURL = Constants.YOUTUBE_TO_MP3_DOWNLOADS_DIRECTORY_URL.appendingPathComponent(filename)
@@ -109,7 +182,7 @@ class VideoPlayerViewController: UIViewController {
                         do {
                             let decodedResponse = try JSONDecoder.decode(YoutubeToMp3Response.self, from: responseData)
                             let fileToDownloadURL = decodedResponse.file
-                            self?.downloadFile(url: URL(string: fileToDownloadURL)!, filename: ("\(decodedResponse.title).mp3"))
+                            self?.downloadMP3(url: URL(string: fileToDownloadURL)!, filename: ("\(decodedResponse.title).mp3"))
                         } catch {
                             print("Error: \(error.localizedDescription)")
                             print(error)
